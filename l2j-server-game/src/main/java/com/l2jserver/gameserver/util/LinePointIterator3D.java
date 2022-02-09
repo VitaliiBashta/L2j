f0d04c0af33595f5f6618de99465031998f5df03
VitaliiBashta <bashta@email.cz>
2022-02-09 09:05:37 +0100

    LinePointIterator3D simplify

--- l2j-server-game/src/main/java/com/l2jserver/gameserver/util/LinePointIterator3D.java
+++ l2j-server-game/src/main/java/com/l2jserver/gameserver/util/LinePointIterator3D.java
@@ -1,140 +1,125 @@
-/*
- * Copyright © 2004-2021 L2J Server
- *
- * This file is part of L2J Server.
- *
- * L2J Server is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * L2J Server is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 package com.l2jserver.gameserver.util;
 
-/**
- * @author HorridoJoho
- */
 public final class LinePointIterator3D {
-	private int _srcX;
-	private int _srcY;
-	private int _srcZ;
-	private final int _dstX;
-	private final int _dstY;
-	private final int _dstZ;
-	private final long _dx;
-	private final long _dy;
-	private final long _dz;
-	private final long _sx;
-	private final long _sy;
-	private final long _sz;
-	private long _error;
-	private long _error2;
-	private boolean _first;
-	
-	public LinePointIterator3D(int srcX, int srcY, int srcZ, int dstX, int dstY, int dstZ) {
-		_srcX = srcX;
-		_srcY = srcY;
-		_srcZ = srcZ;
-		_dstX = dstX;
-		_dstY = dstY;
-		_dstZ = dstZ;
-		_dx = Math.abs((long) dstX - srcX);
-		_dy = Math.abs((long) dstY - srcY);
-		_dz = Math.abs((long) dstZ - srcZ);
-		_sx = srcX < dstX ? 1 : -1;
-		_sy = srcY < dstY ? 1 : -1;
-		_sz = srcZ < dstZ ? 1 : -1;
-		
-		if ((_dx >= _dy) && (_dx >= _dz)) {
-			_error = _error2 = _dx / 2;
-		} else if ((_dy >= _dx) && (_dy >= _dz)) {
-			_error = _error2 = _dy / 2;
-		} else {
-			_error = _error2 = _dz / 2;
-		}
-		
-		_first = true;
-	}
-	
-	public boolean next() {
-		if (_first) {
-			_first = false;
-			return true;
-		} else if ((_dx >= _dy) && (_dx >= _dz)) {
-			if (_srcX != _dstX) {
-				_srcX += _sx;
-				
-				_error += _dy;
-				if (_error >= _dx) {
-					_srcY += _sy;
-					_error -= _dx;
-				}
-				
-				_error2 += _dz;
-				if (_error2 >= _dx) {
-					_srcZ += _sz;
-					_error2 -= _dx;
-				}
-				
-				return true;
-			}
-		} else if ((_dy >= _dx) && (_dy >= _dz)) {
-			if (_srcY != _dstY) {
-				_srcY += _sy;
-				
-				_error += _dx;
-				if (_error >= _dy) {
-					_srcX += _sx;
-					_error -= _dy;
-				}
-				
-				_error2 += _dz;
-				if (_error2 >= _dy) {
-					_srcZ += _sz;
-					_error2 -= _dy;
-				}
-				
-				return true;
-			}
-		} else {
-			if (_srcZ != _dstZ) {
-				_srcZ += _sz;
-				
-				_error += _dx;
-				if (_error >= _dz) {
-					_srcX += _sx;
-					_error -= _dz;
-				}
-				
-				_error2 += _dy;
-				if (_error2 >= _dz) {
-					_srcY += _sy;
-					_error2 -= _dz;
-				}
-				
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	public int x() {
-		return _srcX;
-	}
-	
-	public int y() {
-		return _srcY;
-	}
-	
-	public int z() {
-		return _srcZ;
-	}
+  private final int dstX;
+  private final int dstY;
+  private final int dstZ;
+  private final long dx;
+  private final long dy;
+  private final long dz;
+  private final long sx;
+  private final long sy;
+  private final long sz;
+  private int srcX;
+  private int srcY;
+  private int srcZ;
+  private long error;
+  private long error2;
+  private boolean first;
+
+  public LinePointIterator3D(int srcX, int srcY, int srcZ, int dstX, int dstY, int dstZ) {
+    this.srcX = srcX;
+    this.srcY = srcY;
+    this.srcZ = srcZ;
+    this.dstX = dstX;
+    this.dstY = dstY;
+    this.dstZ = dstZ;
+    dx = Math.abs((long) dstX - srcX);
+    dy = Math.abs((long) dstY - srcY);
+    dz = Math.abs((long) dstZ - srcZ);
+    sx = srcX < dstX ? 1 : -1;
+    sy = srcY < dstY ? 1 : -1;
+    sz = srcZ < dstZ ? 1 : -1;
+
+    if ((dx >= dy) && (dx >= dz)) {
+      error = error2 = dx / 2;
+    } else if ((dy >= dx) && (dy >= dz)) {
+      error = error2 = dy / 2;
+    } else {
+      error = error2 = dz / 2;
+    }
+
+    first = true;
+  }
+
+  public boolean next() {
+    if (first) {
+      first = false;
+      return true;
+    }
+    if ((dx >= dy) && (dx >= dz)) {
+      if (srcX != dstX) {
+        return notSameX();
+      }
+    } else if ((dy >= dx) && (dy >= dz)) {
+      if (srcY != dstY) {
+        return notSameY();
+      }
+    } else if (srcZ != dstZ) {
+      srcZ += sz;
+
+      error += dx;
+      if (error >= dz) {
+        srcX += sx;
+        error -= dz;
+      }
+
+      error2 += dy;
+      if (error2 >= dz) {
+        srcY += sy;
+        error2 -= dz;
+      }
+
+      return true;
+    }
+
+    return false;
+  }
+
+  private boolean notSameY() {
+    srcY += sy;
+
+    error += dx;
+    if (error >= dy) {
+      srcX += sx;
+      error -= dy;
+    }
+
+    error2 += dz;
+    if (error2 >= dy) {
+      srcZ += sz;
+      error2 -= dy;
+    }
+
+    return true;
+  }
+
+  private boolean notSameX() {
+    srcX += sx;
+
+    error += dy;
+    if (error >= dx) {
+      srcY += sy;
+      error -= dx;
+    }
+
+    error2 += dz;
+    if (error2 >= dx) {
+      srcZ += sz;
+      error2 -= dx;
+    }
+    return true;
+  }
+
+  public int x() {
+    return srcX;
+  }
+
+  public int y() {
+    return srcY;
+  }
+
+  public int z() {
+    return srcZ;
+  }
 }
